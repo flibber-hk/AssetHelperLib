b49e740275472b9f6bd1f2b75a67ebccb012f146
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2026-01-09 21:20:07 +0000

    Initial commit

---
+++ AssetHelperLib/Logging.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AssetHelperLib;
+
+/// <summary>
+/// Class used by AssetHelperLib to write logs.
+/// </summary>
+public static class Logging
+{
+    /// <summary>
+    /// On logging an "INFO" level message.
+    /// </summary>
+    public static event Action<string>? OnLog;
+
+    internal static void LogInfo(string message) => OnLog?.Invoke(message);
+
+    /// <summary>
+    /// On logging an "WARNING" level message.
+    /// </summary>
+    public static event Action<string>? OnLogWarning;
+
+    internal static void LogWarning(string message) => OnLogWarning?.Invoke(message);
+
+    /// <summary>
+    /// On logging an "ERROR" level message.
+    /// </summary>
+    public static event Action<string>? OnLogError;
+
+    internal static void LogError(string message) => OnLogError?.Invoke(message);
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# AssetHelperLib
+
+This is a library used to help repack scene assets to new non-scene bundles
+using AssetsTools.NET.
+
+This was built for use in Hollow Knight: Silksong, but may have use
+elsewhere.
